77278f70f965c7ec100d3fccdf95c9b422d78cbb
newor0599 <rowen778623@gmail.com>
2024-04-29 00:02:43 +0800

    script improvment

--- installation.sh
+++ installation.sh
@@ -1,6 +1,15 @@
-echo "newor nvim config installation script"
+clear
+echo "newor nvim config installation script \n"
 
-config_exist = false
+if grep -q "nvim-conf" <<< $(pwd)
+then
+	echo "Starting script"
+else
+	echo "Invalid path (Make sure you are in the cloned folder"
+	exit
+fi
+
+config_exist=false
 
 if [ -n "$(ls -A XDG_CONFIG_HOME/nvim 2>/dev/null)" ]
 then
@@ -9,13 +18,30 @@ fi
 
 if [ config_exist ]
 then
-    echo "Would you like to backup your current neovim config? (y (default)/n) "
+    echo "Would you like to backup your current neovim config? [y (default)/n] "
     read option
 fi
 
-if [ "$option" = "" || "$option" = "y"]
+if [ "$option" = "" ] || [ "$option" = "y" ]
 then
     echo "Backing up config..."
     mv ~/.config/nvim ~/.config/nvim.$(whoami)
     echo "Backup complete"
+else
+    echo "Skip backup procedure"
 fi
+
+echo "Removing neovim config folder"
+rm -rf ~/.config/nvim/*
+
+echo "Removing neovim share folder"
+rm -rf ~/.local/share/nvim
+
+echo "|Copying new config"
+echo "|_init.lua"
+cp -r init.lua ~/.config/nvim
+
+echo "|_lua folder"
+cp -r lua ~/.config/nvim
+
+echo "New config copied"
