11e2c094405306a232e1b14b0aa1230210bd05d8
newor0599 <rowen778623@gmail.com>
2024-04-29 21:05:21 +0800

    NvimTree update cwd

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -3,7 +3,8 @@ return {
     dependencies = "nvim-tree/nvim-web-devicons",
     config = function()
 	require("nvim-tree").setup({
-	    vim.keymap.set("n","<leader>e","<Cmd>NvimTreeFocus<CR>")
+	    vim.keymap.set("n","<leader>e","<Cmd>NvimTreeFocus<CR>"),
+	    update_cwd = true
 	})
 	vim.api.nvim_create_autocmd({"QuitPre"}, {
 	    callback = function() vim.cmd("NvimTreeClose") end,
